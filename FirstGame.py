342a6d70373d1bf171813e8d7bf58134c0787b1e
Chotalia <rchotalia@dxc.com>
2020-04-27 13:55:17 +0100

    Animated char left, right and jump as png

--- FirstGame.py
+++ FirstGame.py
@@ -2,24 +2,63 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500, 480))
 
 pygame.display.set_caption("First Game")
 
+# Each of the images 64x64 pixels and there are 9 each for walking right and left
+walkRight = [pygame.image.load('pics/R1.png'), pygame.image.load('pics/R2.png'), pygame.image.load('pics/R3.png'), pygame.image.load('pics/R4.png'), pygame.image.load(
+    'pics/R5.png'), pygame.image.load('pics/R6.png'), pygame.image.load('pics/R7.png'), pygame.image.load('pics/R8.png'), pygame.image.load('pics/R9.png')]
+walkLeft = [pygame.image.load('pics/L1.png'), pygame.image.load('pics/L2.png'), pygame.image.load('pics/L3.png'), pygame.image.load('pics/L4.png'), pygame.image.load(
+    'pics/L5.png'), pygame.image.load('pics/L6.png'), pygame.image.load('pics/L7.png'), pygame.image.load('pics/L8.png'), pygame.image.load('pics/L9.png')]
+bg = pygame.image.load('pics/bg.jpg')
+char = pygame.image.load('pics/standing.png')
+
 screenWidth = 500
+frameRate = 27  # Because we have this many sprites
+clock = pygame.time.Clock()
 
 x = 50
-y = 450
-width = 40
-height = 60
+y = 400
+width = 64
+height = 64
 vel = 5
 
 isJump = False
 jumpCount = 10
+left = False
+right = False
+walkCount = 0
 
+
+def redrawGameWindow():
+    global walkCount
+    win.blit(bg, (0, 0))
+    # pygame.draw.rect(win, (255, 0, 0), (x, int(y), width, height))
+
+    if walkCount + 1 >= 27:
+        walkCount = 0
+
+    if left:
+        win.blit(walkLeft[int(walkCount//3)], (int(x), int(y)))
+        walkCount += 1
+    elif right:
+        win.blit(walkRight[int(walkCount//3)], (int(x), int(y)))
+        walkCount += 1
+    else:
+        win.blit(char, (int(x), int(y)))
+        walkCount = 0
+
+    pygame.display.update()
+
+
+# main loop
 run = True
 while run:
-    pygame.time.delay(100)  # milliseconds
+    # pygame.time.delay(100)  # milliseconds
+
+    clock.tick(27)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -28,19 +67,24 @@ while run:
 
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-
-    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
+        left = False
+        right = True
+    else:
+        right = False
+        left = False
+        walkCount = 0
 
     if not(isJump):
-        if keys[pygame.K_UP] and y > vel:
-            y -= vel
-
-        if keys[pygame.K_DOWN] and y < screenWidth - height - vel:
-            y += vel
 
         if keys[pygame.K_SPACE]:
             isJump = True
+            right = False
+            left = False
+            walkCount = 0
     else:
         if jumpCount >= -10:
             neg = 1
@@ -52,9 +96,6 @@ while run:
             isJump = False
             jumpCount = 10
 
-    win.fill((0, 0, 0))
-
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
+    redrawGameWindow()
 
 pygame.quit()
